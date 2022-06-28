3e3ed0309cec077ee34f3de6b725e333f84f73b8
tomvieir <wevertonbvieira@gmail.com>
2022-06-27 22:10:19 -0300

    att

--- app.js
+++ app.js
@@ -4,48 +4,93 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
-    carrinhoTotal: 0
+
+
   },
+
   methods: {
-    fetchProdutos() {
+    fetchProdutos() {  // função para buscar os produtos
       fetch("./api/produtos.json")
         .then(r => r.json())
         .then(r => {
           this.produtos = r;
-        })  
+        })
     },
 
-    fetchProduto(id) {
+    fetchProduto(id) {  //chama o produto pelo id
       fetch(`./api/produtos/${id}/dados.json`)
         .then(r => r.json())
         .then(r => {
           this.produto = r;
-        })  
+        })
     },
 
-    fecharModal({target, currentTarget}) {
-      if (target === currentTarget) 
+    fecharModal({ target, currentTarget }) {  //target é o elemento que foi clicado, currentTarget é o elemento que está sendo clicado
+      if (target === currentTarget)
         this.produto = false;
     },
 
-    abrirModal(id) {
+    abrirModal(id) {  // abrir modal
       this.fetchProduto(id);
     },
 
-    adicionarItem() {
-      this.produto.estoque--;
-      const {id, nome, preco} = this.produto;
-      this.carrinho.push({id, nome, preco});
-    }, 
+    adicionarItem() {  // adicionar item ao carrinho
+      this.produto.estoque--;  
+      const { id, nome, preco } = this.produto; 
+      this.carrinho.push({ id, nome, preco });  
+    },
 
-    removerItem(index) {
+    removerItem(index) {  //remove o item do carrinho
       this.carrinho.splice(index, 1);
-    }
+    },
 
+    checarLocalStorage() {  // checa se o localStorage está vazio ou não e se não estiver, recupera os dados do localStorage
+      if (window.localStorage.carrinho)
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+    },
+
+    router() {  
+      const hash = document.location.hash;
+      if (hash) {
+        const id = hash.replace("#", "");
+        this.fetchProduto(id);
+      }     
+    }
   },
-  
+
+
+
+
+  watch: {
+    carrinho() {  //salva os dados do carrinho no localStorage
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    },
+
+    produto() {  // quando o produto é alterado muda o hash do url
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+    }
+  },
+
+
+
+  computed: {
+    carrinhoTotal() {  // calcula o total do carrinho
+
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+
+      return total;
+    },
+  },
+
   filters: {
-    formatarPreco(preco) {
+    formatarPreco(preco) {  // formata o preço
       return preco.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL",
@@ -53,8 +98,10 @@ const vm = new Vue({
       });
     }
   },
-  
-  created() {    
+
+  created() {    // chama as funções que buscam os produtos e checa o localStorage
     this.fetchProdutos();
+    this.checarLocalStorage();
+    this.router();
   }
 })
